d6aa1ab21cf75325fb9c0e58101f8bac194dea83
Gojard Guillaume <ggojard@gmail.com>
2016-12-20 14:50:07 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+// console.log('May Node be with you')
+
+const express = require('express')
+const bodyParser= require('body-parser')
+const app = express()
+const MongoClient = require('mongodb').MongoClient
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+var db
+
+MongoClient.connect('mongodb://expmongo-admin:ognompxe@ds141328.mlab.com:41328/expmongo-quotes', (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  app.listen(3000, () => {
+    console.log('listening on 3000')
+  })
+})
+
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+  // Note: __dirname is directory that contains the JavaScript source code. Try logging it and see what you get!
+  // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
+})
+
+app.post('/quotes', (req, res) => {
+  // console.log('Hellooooooooooooooooo!')
+  // console.log(req.body)
+
+  db.collection('quotes').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+
+    console.log('saved to database')
+    res.redirect('/')
+  })
+
+})
