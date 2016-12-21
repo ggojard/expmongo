5d5c6b47fd6e696bb10f48af56c0fc9eaf826495
Gojard Guillaume <ggojard@gmail.com>
2016-12-21 17:27:20 +0100

    change css

--- public/main.js
+++ public/main.js
@@ -1,5 +1,5 @@
 // main.js
-var update = document.getElementById('update')
+var update = document.getElementById('update');
 
 update.addEventListener('click', function () {
   // Send PUT Request here
