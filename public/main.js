3548f0ec1f0dc9ab2f3fc237ed1f6c18aa4c8d1e
Gojard Guillaume <ggojard@gmail.com>
2016-12-21 17:49:36 +0100

    add timestamp in post

--- public/main.js
+++ public/main.js
@@ -3,7 +3,6 @@ var update = document.getElementById('update');
 
 update.addEventListener('click', function () {
   // Send PUT Request here
-
   fetch('quotes', {
   method: 'put',
   headers: {'Content-Type': 'application/json'},
@@ -19,6 +18,13 @@ update.addEventListener('click', function () {
   	console.log(data)
   	window.location.reload(true)
   })
+})
 
 
+var quote_text = document.getElementById('text');
+var quote_timestamp = document.getElementById('timestamp');
+var now = Date();
+
+update.addEventListener('change', function () {
+	quote_timestamp.value = now;
 })
