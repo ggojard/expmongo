d51916dbfe496fb2f3f43d59523beccb6868e43b
Gojard Guillaume <ggojard@gmail.com>
2016-12-22 16:11:39 +0100

    update method

--- public/js/main.js
+++ public/js/main.js
@@ -66,7 +66,7 @@ $(document).ready(function(){
           })
           .then(data => {
             window.location.reload(true)
-            console.log( JSON.stringify(data));
+            console.log( data);
           })
     });
 
